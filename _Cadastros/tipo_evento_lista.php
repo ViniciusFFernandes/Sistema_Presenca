1aa113d8fcb3c9e59c23393de79afd9314272fa0
leo <leo.intis@gmail.com>
2020-06-11 16:48:50 -0300

    Evento: Cadastro atualizado.

---
+++ _Cadastros/tipo_evento_lista.php
@@ -0,0 +1,80 @@
+<?php
+  include_once("../_BD/conecta_login.php");
+?>
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Sistema de Presença</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    </head>
+    <script>
+    </script>  
+    <body>
+        <?php
+            include_once("../menu.php")
+        ?>
+        <div class="container">
+          <div class="card">
+            <div class="card-header bg-primary text-light">
+              <b>Lista de Tipo de Evento</b>
+              <span class="float-right light"><a href="../_Cadastros/tipo_evento_edita.php"><img src="../icones/adiciona.png" width="28px"></a></span>
+            </div>
+            <div class="card-body">
+              <form action="tipo_evento_lista.php" method="post" id="form_lista">
+                <div class="row">
+                	<div class="col-12 col-sm-12 p-0">
+                		<div class="input-group mb-2">
+							<input type="text" class="form-control" placeholder="Pesquisar Tipo de Evento..." id="pesquisaTipoEvento" name="pesquisaTipoEvento" value="<?= $_REQUEST['pesquisaTipoEvento']?>">
+							<div class="input-group-append">
+								<button type="submit" class="btn"><img src="../icones/lupa.png" width="24px"></button>
+							</div>
+					</div>
+                	</div>
+                </div>
+              </form>
+            	<?php
+					$sql = "SELECT * FROM tipos_eventos";
+					if(!empty($_REQUEST['pesquisaTipoEvento'])){
+						$sql .= " WHERE tiev_descricao LIKE '%" . $_REQUEST['pesquisaTipoEvento'] . "%'";
+					}
+					$res = $db->consultar($sql);
+				?>
+				<div class="row">
+					<div class="col-12 col-sm-12 p-0">
+						<table id="tableTipoEvento" class="table table-striped m-0 p-0" style="font-size: 14px;">
+							<thead class="table-light">
+								<tr>
+									<th>Evento</th>
+								</tr>
+							</thead>
+							<tbody>
+							<?php
+							if(empty($res)){
+							?>
+								<tr><td colspan="2" align="center">Não existem tipos de eventos cadastrados!</td></tr>
+							<?php
+							}
+							foreach($res as $reg){
+							?>
+								<tr onclick="location.href = '../_Cadastros/tipo_evento_edita.php?tiev_id=<?= $reg['tiev_id']?>';" style="cursor: pointer;">
+									<td><?= $reg['tiev_descricao'] ?></td>
+								</tr>
+							<?php
+							}
+							?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+            </div>
+          </div>
+            <?php
+                include_once('../rodape.php');
+            ?>
+        </div>
+    </body>
+</html>

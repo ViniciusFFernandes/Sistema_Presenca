71737733d011f50563f01a87f63f50ce1cba2d80
vinicius <vnferna@gmail.com>
2020-06-10 22:49:38 -0300

    Banco de dados: criação da tabela eventos

--- _BD/SistemadeAcesso.sql
+++ _BD/SistemadeAcesso.sql
@@ -1,15 +1,26 @@
 
 Create table alunos (
 	alu_id Int NOT NULL AUTO_INCREMENT,
-	alu_nome Varchar(100),
-	alu_curso Varchar(100),
-	alu_email Varchar(100),
+	alu_nome Varchar(40),
+	alu_curso Varchar(30),
+	alu_email Varchar(50),
  Primary Key (alu_id)) ENGINE = INNODB;
 
  Create table tipos_eventos (
 	tiev_id Int NOT NULL AUTO_INCREMENT,
-	tiev_descricao Varchar(100),
+	tiev_descricao Varchar(30),
  Primary Key (tiev_id)) ENGINE = INNODB;
+
+  Create table eventos (
+	ev_id Int NOT NULL AUTO_INCREMENT,
+	ev_tiev_id Int NOT NULL,
+	ev_nome Varchar(30),
+	ev_responsavel Varchar(30),
+	ev_horas Int,
+	ev_data date,
+	ev_hora_inicio time,
+	ev_hora_fim time,
+ Primary Key (ev_id)) ENGINE = INNODB;
 
 
 -- Alter table Alunos add Foreign Key (sis_id) references Sistema (sis_id) on delete  restrict on update  restrict;

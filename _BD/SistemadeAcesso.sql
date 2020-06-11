27f2c694a4d640d8ad4010a3acf9035a998ce0d7
vinicius <vnferna@gmail.com>
2020-06-10 21:00:25 -0300

    Banco de dados: Criação das tabelas e alteração de campos existentes

--- _BD/SistemadeAcesso.sql
+++ _BD/SistemadeAcesso.sql
@@ -1,10 +1,15 @@
 
-Create table Alunos (
-	alunos_id Int NOT NULL AUTO_INCREMENT,
-	nome_alu Varchar(100),
-	curso_alu Varchar(100),
-	email_alu Varchar(100),
- Primary Key (alunos_id)) ENGINE = INNODB;
+Create table alunos (
+	alu_id Int NOT NULL AUTO_INCREMENT,
+	alu_nome Varchar(100),
+	alu_curso Varchar(100),
+	alu_email Varchar(100),
+ Primary Key (alu_id)) ENGINE = INNODB;
+
+ Create table tipos_eventos (
+	tiev_id Int NOT NULL AUTO_INCREMENT,
+	tiev_descricao Varchar(100),
+ Primary Key (tiev_id)) ENGINE = INNODB;
 
 
 -- Alter table Alunos add Foreign Key (sis_id) references Sistema (sis_id) on delete  restrict on update  restrict;

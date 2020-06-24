c47b286fe211c34fbf8ffa0284559769b41c2347
vinicius <vnferna@gmail.com>
2020-06-24 17:49:10 -0300

    adicionado comentarios aos codigos e remoção de trechos e paginas desnecessarias

--- _Cadastros/evento_grava.php
+++ _Cadastros/evento_grava.php
@@ -5,12 +5,15 @@
     include_once('../PHPMailer/SMTP.php');
     include_once('../PHPMailer/Exception.php');
     include_once('../phpqrcode/qrlib.php');
-
+    //
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
-
+    //
+    //Rotina usada para alterar e inserir novos registros
     if($_POST['operacao'] == 'Gravar'){
+        //
+        //Validações antes de executar a rotina
         if(empty($_POST['ev_nome'])){
             mostraErro("O nome do evento não pode ser informado em branco!", "Gravar");
         }
@@ -30,6 +33,7 @@
             mostraErro("O horario de fim do evento não pode ser informado em branco!", "Gravar");
         }
         //
+        //inicio da rotina para inserir ou alterar os registros
         $db->setTabela("eventos", "ev_id");
         //
         $dados['id']                = $_POST['ev_id'];
@@ -43,9 +47,13 @@
         $db->gravarInserir($dados);
         //
         if($db->erro()){
+            //
+            //Caso a rotina der error retorna com uma mensagem de erro
             header("Location: ../_Cadastros/evento_edita.php?msg=Erro%20ao%20gravar%20evento&msgTipo=erro");
             exit;
         }else{
+            //
+            //Caso a rotorina der certo define qual o id que deve retornar e reotrna com uma mensagem de sucesso
             if ($_POST['ev_id'] > 0) {
                 $id = $_POST['ev_id'];
             }else{
@@ -55,134 +63,170 @@
             exit;
         }
     }
-
-        if($_POST['operacao'] == 'Excluir'){
-            $db->setTabela("eventos", "ev_id");
-            $db->excluir($_POST['ev_id']);
+    //
+    //Rotina usada para exclusão de registros
+    if($_POST['operacao'] == 'Excluir'){
+        //
+        //Inicio da rotina
+        $db->setTabela("eventos", "ev_id");
+        $db->excluir($_POST['ev_id']);
+        //
+        if($db->erro()){
+            //
+            //Caso a rotina der error retorna uma mensagem de erro
+            header("Location: ../_Cadastros/eve'nto_edita.php?ev_id=" . $_POST['ev_id'] . "&msg=Erro%20ao%20excluir%20evento&msgTipo=erro");
+            exit;
+        }else{
+            //
+            //Caso a rotorina der certo retorna com uma mensagem de sucesso
+            header("Location: ../_Cadastros/evento_edita.php?msg=Evento%20excluido%20com%20sucesso");
+            exit;
+        }
+    }
+    //
+    //Rotina usada para incluir alunos nos eventos
+    if($_POST['operacao'] == 'incluir_alunos'){
+        //
+        //Validações antes de executar a rotina
+        $sql = "SELECT * FROM eventos WHERE ev_id = " . $_POST['ev_id'];
+        $reg = $db->retornaUmReg($sql);
+        if(strtotime($reg['ev_data'] . " " . $reg['ev_hora_fim']) <= strtotime(date("Y-m-d H:i"))){
+            mostraErro("Não é permitido inserir alunos com o evento já finalizado!", "Inserir");
+        }
+        //
+        //Inicio da rotina
+        $db->setTabela("presencas_eventos", "prev_id");
+        foreach($_POST['checkbox_alu_id'] AS $alu_id){
+            unset($dados);
+            //
+            $dados['id']                = 0;
+            $dados['prev_alu_id']       = igr($alu_id);
+            $dados['prev_ev_id'] 	    = igr($_POST['ev_id']);
+            $db->gravarInserir($dados);
             //
             if($db->erro()){
-                header("Location: ../_Cadastros/evento_edita.php?ev_id=" . $_POST['ev_id'] . "&msg=Erro%20ao%20excluir%20evento&msgTipo=erro");
-                exit;
-            }else{
-                header("Location: ../_Cadastros/evento_edita.php?msg=Evento%20excluido%20com%20sucesso");
+                //
+                //Caso a rotina der error retorna uma mensagem de erro
+                header("Location: ../_Cadastros/evento_edita.php?ev_id=" . $_POST['ev_id'] . "msg=Erro%20ao%incluir%20alunos%20no%20evento&msgTipo=erro");
                 exit;
             }
         }
-
-        if($_POST['operacao'] == 'incluir_alunos'){
-            //print_r($_POST);
-            $sql = "SELECT * FROM eventos WHERE ev_id = " . $_POST['ev_id'];
-            $reg = $db->retornaUmReg($sql);
-            if(strtotime($reg['ev_data'] . " " . $reg['ev_hora_fim']) <= strtotime(date("Y-m-d H:i"))){
-                mostraErro("Não é permitido inserir alunos com o evento já finalizado!", "Inserir");
-            }
+        //
+        //Caso a rotorina der certo retorna com uma mensagem de sucesso
+        header("Location: ../_Cadastros/evento_edita.php?ev_id=" . $_POST['ev_id'] . "&msg=Alunos%20incluidos%20no%20evento");
+        exit;
+    }
+    //
+    //Rotina usada para exclusão de registros
+    if($_POST['operacao'] == 'excluirMatricula'){
+        //
+        //Inicio da rotina
+        $db->setTabela("presencas_eventos", "prev_id");
+        $db->excluir($_POST['prev_id']);
+        //
+        if($db->erro()){
             //
-            $db->setTabela("presencas_eventos", "prev_id");
-            foreach($_POST['checkbox_alu_id'] AS $alu_id){
-                unset($dados);
+            //Caso a rotina der error retorna "Erro" para o javaScript exibir que não foi possivel excluir
+            echo "Erro";
+        }else{
+            //
+            //Caso a rotina der certo retorna "Ok" para o javaScript exibir que a foi excluido e remover da tela
+            echo "Ok";
+        }
+        exit;
+    }
+    //
+    //Rotina usada para gerar e enviar o QR Code de cada aluno por e-mail
+    if($_POST['operacao'] == 'gerarQR'){
+        //
+        //Inicio da rotina
+        $sql = "SELECT * FROM presencas_eventos 
+                    JOIN alunos ON (prev_alu_id = alu_id) 
+                    JOIN eventos ON (prev_ev_id = ev_id)
+                WHERE prev_ev_id = " . $_POST['ev_id'];
+        if($_POST['prev_id'] != ''){
+            $sql .= " AND prev_id = " . $_POST['prev_id'];
+        }
+        //
+        //Efetua a leitura de todos ou do aluno que deve ser gerado e enviado o QR Code
+        $res = $db->consultar($sql);
+        foreach($res AS $reg){
+            //     
+            //Biblioteca usada para gerar o QR Code e salvar a imagem              
+            QRcode::png($reg['prev_id'], "qrcode_tmp.png", QR_ECLEVEL_H, 10);
+            //
+            //Neste ponto define-se o nome da imagem no e-mail e o corpo do mesmo 
+            $cid = date('YmdHms').'.'.time();
+            //
+            $texto="
+            <html>
+            <body>
+            <div>
+            <font size=3>
+            Ola {$reg['alu_nome']},
+            </font>
+            </div>
+            <div>
+            <font size=3>
+            Segue a baixo seu QR Code de autetificação para o evento {$reg['ev_nome']} 
+            </font>
+            </div>
+            </ br>
+            </ br>
+            <div align='center'>
+            <img src=\"cid:$cid\">
+            </div>
+            <div>
+            <font size=2>
+            Não esqueça de comparecer ao evento com este e-mail em mãos!
+            </font>
+            </div>
+            </body>
+            </html>
+            ";
+            //
+            //Inicia a classe para envio de e-mail
+            $mail = new PHPMailer();
+            //
+            //Validação do envio
+            try {
                 //
-                $dados['id']                = 0;
-                $dados['prev_alu_id']       = igr($alu_id);
-                $dados['prev_ev_id'] 	    = igr($_POST['ev_id']);
-                $db->gravarInserir($dados);
+                //Inicia a configuracões do servidor smtp (configuração feitas pelo constante.php)
+                $mail->isSMTP();
+                $mail->Host = $SMTP_HOST;
+                $mail->SMTPAuth = true;
+                $mail->Username = $SMTP_LOGIN;
+                $mail->Password = $SMTP_SENHA;
+                $mail->Port = $SMTP_PORTA;
                 //
-                if($db->erro()){
-                    header("Location: ../_Cadastros/evento_edita.php?ev_id=" . $_POST['ev_id'] . "msg=Erro%20ao%incluir%20alunos%20no%20evento&msgTipo=erro");
+                //Define o e-mail de envio e para qual sera enviado
+                $mail->setFrom($SMTP_EMAIL);
+                $mail->addAddress($reg['alu_email']);
+                //
+                //Adicona a imagem como anexo para ser exibida no corpo
+                $mail->AddEmbeddedImage("qrcode_tmp.png",$cid,$cid);
+                $mail->isHTML(true);
+                $mail->Subject = "QR Code do Evento " . $reg['ev_nome'];
+                $mail->Body = $texto;
+                //
+                //Verifica se o email foi enviado
+                if(!$mail->send()) {
+                    echo 'Erro';
+                    unlink("qrcode_tmp.png");
                     exit;
                 }
+            } catch (Exception $e) {
+                //
+                //Executado caso ocorra algum erro na classe de envio
+                echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+                unlink("qrcode_tmp.png");
+                exit;
             }
             //
-            header("Location: ../_Cadastros/evento_edita.php?ev_id=" . $_POST['ev_id'] . "&msg=Alunos%20incluidos%20no%20evento");
-            exit;
+            //Apaga a imagem gerada pois o sistema não armazena os QR Codes
+            unlink("qrcode_tmp.png");
         }
-
-        if($_POST['operacao'] == 'excluirMatricula'){
-            $db->setTabela("presencas_eventos", "prev_id");
-            $db->excluir($_POST['prev_id']);
-            //
-            if($db->erro()){
-                echo "Erro";
-            }else{
-                echo "Ok";
-            }
-            exit;
-        }
-
-        if($_POST['operacao'] == 'gerarQR'){
-        
-                $sql = "SELECT * FROM presencas_eventos 
-                            JOIN alunos ON (prev_alu_id = alu_id) 
-                            JOIN eventos ON (prev_ev_id = ev_id)
-                        WHERE prev_ev_id = " . $_POST['ev_id'];
-                if($_POST['prev_id'] != ''){
-                    $sql .= " AND prev_id = " . $_POST['prev_id'];
-                }
-                // echo $sql;
-                $res = $db->consultar($sql);
-                foreach($res AS $reg){
-                    //                   
-                    QRcode::png($reg['prev_id'], "qrcode_tmp.png", QR_ECLEVEL_H, 10);
-                    //
-                    $cid = date('YmdHms').'.'.time();
-                    //
-                    $texto="
-                    <html>
-                    <body>
-                    <div>
-                    <font size=3>
-                    Ola {$reg['alu_nome']},
-                    </font>
-                    </div>
-                    <div>
-                    <font size=3>
-                    Segue a baixo seu QR Code de autetificação para o evento {$reg['ev_nome']} 
-                    </font>
-                    </div>
-                    </ br>
-                    </ br>
-                    <div align='center'>
-                    <img src=\"cid:$cid\">
-                    </div>
-                    <div>
-                    <font size=2>
-                    Não esqueça de comparecer ao evento com este e-mail em mãos!
-                    </font>
-                    </div>
-                    </body>
-                    </html>
-                    ";
-
-                    $mail = new PHPMailer();
-
-                    try {
-                        $mail->isSMTP();
-                        $mail->Host = $SMTP_HOST;
-                        $mail->SMTPAuth = true;
-                        $mail->Username = $SMTP_LOGIN;
-                        $mail->Password = $SMTP_SENHA;
-                        $mail->Port = $SMTP_PORTA;
-
-                        $mail->setFrom($SMTP_EMAIL);
-                        $mail->addAddress($reg['alu_email']);
-                        
-                        $mail->AddEmbeddedImage("qrcode_tmp.png",$cid,$cid);
-                        $mail->isHTML(true);
-                        $mail->Subject = "QR Code do Evento " . $reg['ev_nome'];
-                        $mail->Body = $texto;
-
-                        if(!$mail->send()) {
-                            echo 'Erro';
-                            unlink("qrcode_tmp.png");
-                            exit;
-                        }
-                    } catch (Exception $e) {
-                        echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
-                        unlink("qrcode_tmp.png");
-                        exit;
-                    }
-                    unlink("qrcode_tmp.png");
-                }
-            echo 'Ok';
-            exit;
-        }
+        echo 'Ok';
+        exit;
+    }
     ?>
--- _Cadastros/tipo_evento_grava.php
+++ _Cadastros/tipo_evento_grava.php
@@ -1,12 +1,16 @@
 <?php
     include_once("../_BD/conecta_login.php");
     include_once("../funcoes/funcoes.php");
-
-     if($_POST['operacao'] == 'Gravar'){
+    //
+    //Rotina usada para alterar e inserir novos registros
+    if($_POST['operacao'] == 'Gravar'){
+        //
+        //Validações antes de executar a rotina
         if(empty($_POST['tiev_descricao'])){
             mostraErro("A descrição do tipo de evento não pode ser informado em branco!", "Gravar");
         }
         //
+        //inicio da rotina para inserir ou alterar os registros
         $db->setTabela("tipos_eventos", "tiev_id");
         //
         $dados['id']                = $_POST['tiev_id'];
@@ -14,9 +18,13 @@
         $db->gravarInserir($dados);
         //
         if($db->erro()){
+            //
+            //Caso a rotina der error retorna com uma mensagem de erro
             header("Location: ../_Cadastros/tipo_evento_edita.php?msg=Erro%20ao%20gravar%20Tipo%20de%20Evento&msgTipo=erro");
             exit;
         }else{
+            //
+            //Caso a rotorina der certo define qual o id que deve retornar e reotrna com uma mensagem de sucesso
             if ($_POST['tiev_id'] > 0) {
                 $id = $_POST['tiev_id'];
             }else{
@@ -26,15 +34,22 @@
             exit;
         }
     }
-
+    //
+    //Rotina usada para exclusão de registros
     if($_POST['operacao'] == 'Excluir'){
+        //
+        //Inicio da rotina
         $db->setTabela("tipos_eventos", "tiev_id");
         $db->excluir($_POST['tiev_id']);
         //
         if($db->erro()){
+            //
+            //Caso a rotina der error retorna com o id e uma mensagem de erro
             header("Location: ../_Cadastros/tipo_evento_edita.php?tiev_id=" . $_POST['tiev_id'] . "&msg=Erro%20ao%20excluir%20Tipo%20de%20Evento&msgTipo=erro");
             exit;
         }else{
+            //
+            //Caso a rotorina der certo retorna com uma mensagem de sucesso
             header("Location: ../_Cadastros/tipo_evento_edita.php?msg=Tipo%20de%20Evento%20excluido%20com%20sucesso");
             exit;
         }
